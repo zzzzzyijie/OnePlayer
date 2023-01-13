66f97e291bb0a470c9860553bbf56b1f8294b89c
zengyijie <zengyijie@jianzhikeji.com>
2023-01-13 17:16:37 +0800

    local commit

---
+++ Example/OnePlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnePlayer
+//
+//  Created by zengyijie on 01/13/2023.
+//  Copyright (c) 2023 zengyijie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // ok then
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
